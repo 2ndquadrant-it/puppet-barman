68593f5033eff2cb862a9377c6c30abeffa7b1af
Alessandro Grassi <alessandro.grassi@devise.it>
2013-06-21 09:53:17 +0200

    Add some checks around barman_key fact to avoid warnings when the module is not used

--- lib/facter/barman_key.rb
+++ lib/facter/barman_key.rb
@@ -1,6 +1,14 @@
 Facter.add('barman_key') do
   setcode do 
-    system('[ -f ~barman/.ssh/id_rsa ] || su - barman -c "ssh-keygen -t rsa -P \"\" -q -f ~barman/.ssh/id_rsa"')
-    Facter::Util::Resolution.exec('/bin/cat /var/lib/barman/.ssh/id_rsa.pub').chomp
+    Etc.passwd { |x|
+      if x.name == 'barman'
+        system('[ -f ~barman/.ssh/id_rsa ] || su - barman -c "ssh-keygen -t rsa -P \"\" -q -f ~barman/.ssh/id_rsa"')
+      end
+    }
+    if File.exists? '/var/lib/barman/.ssh/id_rsa.pub'
+      Facter::Util::Resolution.exec('/bin/cat /var/lib/barman/.ssh/id_rsa.pub').chomp
+    else
+      ''
+    end
   end
 end
