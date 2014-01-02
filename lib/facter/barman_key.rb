fb8473f44234ea600ffad2775e605dc5ef194ef7
Marco Nenciarini <marco.nenciarini@2ndquadrant.it>
2014-01-02 19:14:32 +0100

    Export also postgres_key fact

--- lib/facter/barman_key.rb
+++ lib/facter/barman_key.rb
@@ -1,14 +1,32 @@
-Facter.add('barman_key') do
-  setcode do 
-    Etc.passwd { |x|
-      if x.name == 'barman'
-        system('[ -f ~barman/.ssh/id_rsa ] || su - barman -c "ssh-keygen -t rsa -P \"\" -q -f ~barman/.ssh/id_rsa"')
+require 'etc'
+
+def safe_keygen_and_return (user)
+  Etc.passwd { |entry|
+    if entry.name == user
+      if File.exists? "#{entry.dir}/.ssh/id_rsa.pub"
+        return File.read("#{entry.dir}/.ssh/id_rsa.pub").chomp
+      else
+        Facter::Util::Resolution.exec("su - #{entry.name} -c \"ssh-keygen -t rsa -P '' -q -f #{entry.dir}/.ssh/id_rsa\"")
+        if File.exists? "#{entry.dir}/.ssh/id_rsa.pub"
+          return File.read("#{entry.dir}/.ssh/id_rsa.pub").chomp
+        else
+          return ''
+        end
       end
-    }
-    if File.exists? '/var/lib/barman/.ssh/id_rsa.pub'
-      Facter::Util::Resolution.exec('/bin/cat /var/lib/barman/.ssh/id_rsa.pub').chomp
-    else
-      ''
     end
+  }
+  return ''
+end
+
+
+Facter.add('barman_key') do
+  setcode do
+    safe_keygen_and_return('barman')
+  end
+end
+
+Facter.add('postgres_key') do
+  setcode do
+    safe_keygen_and_return('postgres')
   end
 end
